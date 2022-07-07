075d585883986899d573ff0ab0eca63723549e6c
Edward Chen <echen333us@gmail.com>
2022-07-07 02:04:27 -0400

    basically finished, need better and more data though

--- plotData.py
+++ plotData.py
@@ -1,22 +1,59 @@
 import matplotlib.pyplot as plt
 import csv
 
-with open('dataRet.csv','r')as file:
-   filecontent=csv.reader(file)
-   for row in filecontent:
-       diff3 = int(row[4])
-       diff4 = int(row[5])
-       ret = row[6]
-       print(diff3,diff4, ret)
-       if diff3>50 or diff4>50:
-           continue;
-       
-       if(ret == 'true'):
-           plt.plot([diff3],[diff4],'ro')
-       else :
-           plt.plot([diff3],[abs(diff4)],'b+')
-           
+from numpy import percentile
 
-# plt.plot([1, 2, 3, 4])
-# plt.ylabel('some numbers')
+def plotChance():
+    wins = [0 for i in range(55)]
+    tot = [0 for i in range(55)]
+    plt.xlabel("Point gap after 3rd Q")
+    plt.ylabel("Chance of winning")
+    plt.ylim(0,100);
+
+    with open('dataRet.csv','r')as file:
+        filecontent=csv.reader(file)
+        for row in filecontent:
+            if row[4]=="NaN" or row[5]=="NaN":
+                continue;
+            diff3 = int(row[4])
+            diff4 = int(row[5])
+            ret = row[6]
+            if diff3>50 or abs(diff4)>50:
+                continue;
+            tot[diff3]+=1
+            
+            
+            if ret == 'true':
+                wins[diff3]+=1
+    
+    for i in range(55):
+        if(tot[i]==0):
+            continue;
+        percentage = 100.0*wins[i]/tot[i]; 
+        percentage2 = int(percentage);
+        print(i,percentage2);
+        plt.plot([i],[percentage2],'xb-');
+    
+def plotAllPoints():
+    plt.xlabel("Point gap before 4th Q")
+    plt.ylabel("Point gap after 4th Q")
+    with open('dataRet.csv','r')as file:
+        filecontent=csv.reader(file)
+        for row in filecontent:
+            if row[4]=="NaN" or row[5]=="NaN":
+                continue;
+            diff3 = int(row[4])
+            diff4 = int(row[5])
+            ret = row[6]
+            if diff3>50 or abs(diff4)>50:
+                continue;
+            
+            if(ret == 'true'):
+                plt.plot([diff3],[diff4],'ro', markersize=3)
+            else :
+                plt.plot([diff3],[abs(diff4)],'b+', markersize=3)
+    
+           
+plotChance()
+# plotAllPoints()
 plt.show()
--- server.js
+++ server.js
@@ -4,119 +4,88 @@ const cheerio = require('cheerio');
 const fs = require('fs')
 
 async function scrapeBoxScore (str) {
-    console.log("BYE",str);
-    const text = await axios.get(str);
-    console.log(str);
-    const $ = cheerio.load(text.data);
-
-
-    // const x = $('div[id="div_line_score"]');
-    // const y = $('div[id="line_score_sh"]'); //WORKS
-    // const yy = $('div[id="div_line_score"]').first();
-    // const yyy = $('#all_line_score').find('.div_line_score');
-    // const yya = $('table[id="line_score"]').first();
-    // const zb = $('.suppress_all stats_table');
-    const nn=$('#div_line_score');
-    console.log(nn.html());
-
-    // const x = $('#all_line_score');
-    // console.log(x.html());
-    const xy = $('#all_line_score').each((i,el)=>{
-        console.log("ASDAS");
-        // console.log($(el).html());
-        // console.log("RN",$(el).html());
-        TMPX = ($(el).html());
-        
-        // TMPX.toString().map
-        scoreArr = [];
-        cur = "";
-        last = -1;
-        for (let i in TMPX.toString()) {
-            Tchar = TMPX[i];
-            if (Tchar >='0' && Tchar <='9'){
-                // console.log(i,Tchar);
-                if(last != i-1){
-                    if(cur.length>1 && cur!="2022" && cur!=="2021"){
-                        scoreArr.push(cur);
+    try {
+        const text = await axios.get(str);
+        console.log(str);
+        const $ = cheerio.load(text.data);
+    
+    
+        const xy = $('#all_line_score').each((i,el)=>{
+            TMPX = ($(el).html());
+            
+            scoreArr = [];
+            cur = "";
+            last = -1;
+            for (let i in TMPX.toString()) {
+                Tchar = TMPX[i];
+                if (Tchar >='0' && Tchar <='9'){
+                    if(last != i-1){
+                        if(cur.length>1 && cur!="2022" && cur!=="2021"){
+                            scoreArr.push(parseInt(cur));
+                        }
+                        cur = "";
                     }
-                    cur = "";
+                    cur += TMPX[i];
+                    last = i;
                 }
-                cur += TMPX[i];
-                last = i;
             }
-        }
+            scoreArr.push(parseInt(cur));
+    
+            sum3 = scoreArr[0]+(scoreArr[1])+(scoreArr[2]);
+            sum4 = scoreArr[4];
+            sum32 = scoreArr[5]+(scoreArr[6])+(scoreArr[7]);
+            sum42 = scoreArr[9];
 
-        sum3 = parseInt(scoreArr[0])+parseInt(scoreArr[1])+parseInt(scoreArr[2]);
-        sum4 = parseInt(scoreArr[4]);
-        sum32 = parseInt(scoreArr[5])+parseInt(scoreArr[6])+parseInt(scoreArr[7]);
-        sum42 = parseInt(scoreArr[5])+parseInt(scoreArr[6])+parseInt(scoreArr[7])+parseInt(scoreArr[8]);
-        // dataS = sum3.toString()+sum4+sum32+sum42;
-        qdiff = Math.abs(sum3-sum32);
-        q4Diff = (sum3>sum32? sum4-sum42: sum42-sum4);
-        isWin = q4Diff>0;
-        dataS = [sum3,sum4,sum32,sum42, qdiff, q4Diff, isWin];
-        writeStream.write(dataS.join(',')+ '\n', () => {
-            // a line was written to stream
-        })
-    });
+            if(scoreArr.length===12){
+                console.log("OT");
+                return;
+            }
+            qdiff = Math.abs(sum3-sum32);
+            q4Diff = (sum3>sum32? sum4-sum42: sum42-sum4);
+            isWin = q4Diff>0;
+            dataS = [sum3,sum4,sum32,sum42, qdiff, q4Diff, isWin];
+            writeStream.write(dataS.join(',')+ '\n', () => {
+                // a line was written to stream
+            })
+
+        });
+    } catch (error) {
+        console.log("ERROR@", str);
+    }
 }
-async function scrape () {
+async function scrape (year, month) {
     try {
         const text = await axios.
-            get('https://www.basketball-reference.com/leagues/NBA_2022_games.html');
-            // get('http://paulgraham.com/heresy.html');
-            // get('http://dev.to');
+            // get('https://www.basketball-reference.com/leagues/NBA_2022_games.html');
+            get(`https://www.basketball-reference.com/leagues/NBA_${year}_games-${month}.html`);
         
         const $ = cheerio.load(text.data);
     
-        // $firstImageWithAlt = $('img[alt]').first();
-        // const title = $firstImageWithAlt.toArray()[0].attribs.alt;
-    
-        // console.log(title);
-        // const x = $('.crayons-story__author-pic')
-        // console.log(x.html());
-
-        const x = $('.table_container');
-        const y = $('.table_container').children();
-        const z =  $('.table_container').children().find('tbody').each( (i,el) => {
-            // console.log("HI");
-            // console.log($(el).text());
-        });
-        const za =  $('.table_container').children().find('tbody').find('tr').each( (i,el) => {
-            // console.log("HI");
-            // console.log($(el).children().text());
-        });
-        const zb =  $('.table_container').children().find('tbody').find('tr').find('th').find('a').each( (i,el) => {
-            // console.log("HI");
-            // console.log($(el).attr('href'));
-        });
         const zc =  $('.table_container').children().find('tbody').find('tr').find('td').find('a').each( (i,el) => {
-            // if(i<3){
+            if(i<300){
                 const str = $(el).attr('href');
                 if(str.includes('boxscores')){
-                    scrapeBoxScore(`https://www.basketball-reference.com/${str}`);
+                    scrapeBoxScore(`https://www.basketball-reference.com${str}`);
                     console.log(i,str);
                 }
-            // }
+            }
         });
-        // console.log(y.html());
-
-        
-        // $tmps = $('.crayons-story__author-pic');
-        // const go=tmps.html();
-        // console.log(go);
-    
     } catch(err){
         console.log(err.message);
     }
 }
 
+async function scrapeAll( yearLb, yearUb){
+    const months = ['october', 'november', 'december', 'january', 'february', 'march', 'april', 'may', 'june']
+    // const months = ['november','october','december']
+    for (let i=yearLb;i<=yearUb;i++){
+        for (let j=0;j<months.length;j++){
+            scrape(i, months[j]);
+        }
+    }
+    console.log("Success")
+
+}
 let writeStream = fs.createWriteStream('./dataRet.csv')
-
-
-dataS = ["Team 1 Q3", "Team 1 Total", "Team 2 Q3", "Team 2 Total"];
-writeStream.write(dataS.join(',')+ '\n', () => {
-    // a line was written to stream
-})
-scrape();
+scrapeAll(2022,2022);
 // writeStream.end();
